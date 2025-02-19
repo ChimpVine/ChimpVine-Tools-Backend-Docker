aeb58f26162484c451c4964db29ba29288bad85e
Aayesha Nakarmi <aayeshanakarmi@gmail.com>
2025-02-19 14:53:30 +0545

    recaptcha updated

--- app.py
+++ app.py
@@ -1869,10 +1869,6 @@ def submit_form():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-    
-# if __name__ == '__main__':
-#     app.run(debug=True)
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True)
