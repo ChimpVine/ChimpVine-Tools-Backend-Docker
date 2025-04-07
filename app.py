b6141794d921d8b52533bd9b2539b0a4d5dfa0f2
Aayesha Nakarmi <aayeshanakarmi@gmail.com>
2025-04-07 19:49:17 +0545

    fixed port

--- app.py
+++ app.py
@@ -2260,9 +2260,9 @@ def google_sheet():
         return jsonify({"error": str(e)}), 500
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-    
-    
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True)
+    
+    
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=8080)
